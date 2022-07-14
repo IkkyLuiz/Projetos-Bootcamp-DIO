d416ebf63f891b44b660088db1ba240973824ad1
Luiz Henrique <81529238+IkkyLuiz@users.noreply.github.com>
2022-07-14 15:44:59 -0300

    Add files via upload
    Adicionando Typescript e fazendo modificações importantes no código do projeto, correções de erros e comparação entre Javascript e Typescriot.

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-
-let button = document.getElementById('button'); 
+"use strict";
+let button = document.getElementById('button');
 let input1 = document.getElementById('input1');
 let input2 = document.getElementById('input2');
-
-
-function somarNumeros(numero1, numero2) {
-    if (typeof numero1 === 'number' && typeof numero2 === 'number') {
-        return numero1 + numero2
-    } else {
-        return Number(numero1) + Number(numero2)
-    }
+function adicionarNumeros(numero1, numero2) {
     return numero1 + numero2;
 }
-
-button.addEventListener('click', () => {
-    console.log(somarNumeros(input1.value, input2.value));
-});
-
-
+if (button) {
+    button.addEventListener('click', () => {
+        if (input1 && input2) {
+            console.log(adicionarNumeros(Number(input1.value), Number(input2.value)));
+        }
+    });
+}
